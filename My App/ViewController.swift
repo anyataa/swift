24896fcd02cf653cd94aa886a6a5dae7a7f640eb
Anya Tamara Akbar <anyatmr@gmail.com>
2021-03-20 19:21:15 +0700

    simple app - my first app

--- My App/ViewController.swift
+++ My App/ViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var circularImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        circularImage.layer.cornerRadius = circularImage.frame.size.width/2
+        circularImage.clipsToBounds = true
         // Do any additional setup after loading the view.
     }
 
